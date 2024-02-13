642080b015a642b49bcf5832a658be4c69484de2
Gauresh G Pai <paigauresh@gmail.com>
2024-02-13 06:20:21 +0530

    light-dark mode demo

---
+++ css components/light-dark mode/script.js
@@ -0,0 +1,6 @@
+function toggle() {
+    const body = document.body;
+    const currentTheme = body.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    body.setAttribute('data-theme', newTheme);
+}
