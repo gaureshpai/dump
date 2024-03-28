13fec8abc5c93d1bbe230cd45cec6022381407cb
paigauresh <paigauresh@gmail.com>
2024-03-28 08:49:30 +0530

    Javascript Build a Calculator using HTML, CSS and Javascript done!!

---
+++ Javascript Build a Calculator using HTML, CSS and Javascript/calculator.js
@@ -0,0 +1,43 @@
+    function add() {
+            var act = mainform.calculator.value;
+    var b = act.charAt(act.length - 1);
+    if (b == '+' || b == '-' || b == '*' || b == '/') {
+        mainform.calculator.value = act.substring(0, act.length - 1);
+    mainform.calculator.value += '+';
+            } else {
+        mainform.calculator.value += '+';
+            }
+        }
+
+    function sub() {
+            var act = mainform.calculator.value;
+    var b = act.charAt(act.length - 1);
+    if (b == '+' || b == '-' || b == '*' || b == '/') {
+        mainform.calculator.value = act.substring(0, act.length - 1);
+    mainform.calculator.value += '-';
+            } else {
+        mainform.calculator.value += '-';
+            }
+        }
+
+    function mul() {
+            var act = mainform.calculator.value;
+    var b = act.charAt(act.length - 1);
+    if (b == '+' || b == '-' || b == '*' || b == '/') {
+        mainform.calculator.value = act.substring(0, act.length - 1);
+    mainform.calculator.value += '*';
+            } else {
+        mainform.calculator.value += '*';
+            }
+        }
+
+    function div() {
+            var act = mainform.calculator.value;
+    var b = act.charAt(act.length - 1);
+    if(b == '+' || b == '-' || b == '*' || b == '/') {
+        mainform.calculator.value = act.substring(0, act.length - 1);
+    mainform.calculator.value += '/';
+            } else {
+        mainform.calculator.value += '/';
+            }
+        }
