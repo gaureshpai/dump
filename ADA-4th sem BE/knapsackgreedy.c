6928396c7d709e19fe284e59d9d1a594c1a9acb7
paigauresh <paigauresh@gmail.com>
2024-07-17 14:31:21 +0530

    code correction

--- ADA-4th sem BE/knapsackgreedy.c
+++ ADA-4th sem BE/knapsackgreedy.c
@@ -6,23 +6,29 @@ int main(){
     printf("Enter the number of items:");
     scanf("%d",&n);
     for(i=0;i<n;i++){
+        printf("Enter the weight and profit of item[%d] ", i);
+        scanf("%f%f",&weight[i],&profit[i]);
+    }
+    printf("Enter the capacity:");
+    scanf("%f", &capacity);
+    for(i=0;i<n;i++){
         ratio[i]=profit[i]/weight[i];
     }
     for(i=0;i<n;i++)
         for(j=i+1;j<n;j++)
-        if(ratio[i]<ratio[j]){
-            temp = ratio[j];
-            ratio[j] = ratio[i];
-            ratio[i] = temp;
+            if(ratio[i]<ratio[j]){
+                temp = ratio[j];
+                ratio[j] = ratio[i];
+                ratio[i] = temp;
 
-            temp = weight[j];
-            weight[j] = weight[i];
-            weight[i] = temp;
+                temp = weight[j];
+                weight[j] = weight[i];
+                weight[i] = temp;
 
-            temp = profit[j];
-            profit[j] = profit[i];
-            profit[i] = temp;
-        }
+                temp = profit[j];
+                profit[j] = profit[i];
+                profit[i] = temp;
+            }
     printf("Knapsack problems using Greedy Algorithm:\n");
     for(i=0;i<n;i++)
         if(weight[i]>capacity)
