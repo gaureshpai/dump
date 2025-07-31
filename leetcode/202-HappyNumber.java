54c42a090722da88ee9f525f6347d815f54a29a1
paigauresh <paigauresh@gmail.com>
2025-07-31 23:03:55 +0530

    Leetcode day 10 code upload - 4

---
+++ leetcode/202-HappyNumber.java
@@ -0,0 +1,28 @@
+/*
+Question:
+Write an algorithm to determine if a number n is happy.
+A happy number is a number defined by the following process:
+- Starting with any positive integer, replace the number by the sum of the squares of its digits.
+- Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+- Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+*/
+
+class Solution {
+    public int happy(int n){
+        int num=0;
+        while(true){
+            if(n==0) return num;
+            num += (n%10)*(n%10);
+            n=n/10;
+        }
+    }
+    public boolean isHappy(int n) {
+        Set<Integer> x = new HashSet<>();
+        while(true){
+            n = happy(n);
+            if(n==1) return true;
+            if(!x.add(n)) return false; 
+        }
+    }
+}
---
+++ leetcode/9-PalindromeNumber.java
@@ -0,0 +1,35 @@
+/*
+Question:
+Given an integer x, return true if x is a palindrome, and false otherwise.
+*/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int num=x;
+        int rev=0;
+        while(x!=0){
+            rev = rev*10+x%10;
+            x=x/10;
+        }
+        return num==rev;
+    }
+}
+
+/*
+First Thought:
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        String y = String.valueOf(x);
+        String m = "";
+        for(int i=y.length()-1;i>=0;i--){
+            m+=y.charAt(i);
+        }
+        for(int i=y.length()-1;i>=0;i--){
+            if(y.charAt(i)!=m.charAt(i)) return false;
+        }
+        return true;
+    }
+}
+*/
