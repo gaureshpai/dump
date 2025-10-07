fada0f34adde07cd9a8f8e71c68779c35e4050a4
gaureshpai <paigauresh@gmail.com>
2025-10-07 22:29:48 +0530

    Leetcode day 78 code upload - 2

--- leetcode/1491-AverageSalaryExcludingtheMinimumandMaximumSalary.java
+++ leetcode/1491-AverageSalaryExcludingtheMinimumandMaximumSalary.java
@@ -2,7 +2,7 @@
 Question:
 You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
 Return the average salary of employees excluding the minimum and maximum salary. 
-Answers within 10-5 of the actual answer will be accepted.
+Answers within 10^-5 of the actual answer will be accepted.
 */
 
 class Solution {
---
+++ leetcode/182-DuplicateEmails.sql
@@ -0,0 +1,18 @@
+/*
+Question:
+Table: Person
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+Return the result table in any order.
+The result format is in the following example.
+*/
+
+/*Write your MySQL query statement below*/
+SELECT EMAIL FROM PERSON GROUP BY EMAIL HAVING COUNT(*) >= 2
