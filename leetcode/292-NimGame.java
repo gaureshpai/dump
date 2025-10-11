dcc7798f315e9914b4043ef53a15c98080a7d7d3
gaureshpai <paigauresh@gmail.com>
2025-10-11 23:25:53 +0530

    Leetcode day 82 code upload - 1

---
+++ leetcode/292-NimGame.java
@@ -0,0 +1,15 @@
+/*
+Question:
+You are playing the following Nim Game with your friend:
+- Initially, there is a heap of stones on the table.
+- You and your friend will alternate taking turns, and you go first.
+- On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+- The one who removes the last stone is the winner.
+Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.
+*/
+
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
